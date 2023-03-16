59db94b5cdd69217a67da40c754756ef5cb21b9a
Aino Kytölä <aino.kytola@almamedia.fi>
2023-03-16 08:31:36 +0200

    init commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+init = () => {
+
+  console.log('%cWebsite created with <3 by Aino Kytölä © 2023', 'color:#ff6ec7;font-size:20px;font-weight:bold')
+  const heading = document.getElementById('heading')
+
+  const onClick = () => {
+    heading.addEventListener(onClick)
+  }
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
